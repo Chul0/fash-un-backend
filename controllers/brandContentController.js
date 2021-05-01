52bc0bfe1b16383bd7ccc3c2062c53754f2eced4
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-05-01 17:21:30 -0400

    saving content to my board

--- controllers/brandContentController.js
+++ controllers/brandContentController.js
@@ -20,6 +20,24 @@ brandContentController.getBrandContent = async (req, res) => {
 }
 
 
+brandContentController.save = async (req, res) => {
+    try {
+        const brandContent = await models.brandContent.findOne({
+            where:{
+                id:req.params.id
+            }
+        })
+
+        const user = await models.user.findOne({
+            where: {id: req.headers.authorization }
+        })
+
+        let addAssociation = await user.addBrandContent(brandContent)
+        res.json({brandContent, user, addAssociation})
+    } catch (error) {
+        res.json(error)
+    }
+}
 
 
 module.exports =brandContentController
